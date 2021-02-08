d1c72471b8b66f200a9c1a361ebe1b59a04d75c5
Rafael Oliveira pires <rafaelo.oliveira2020@gmail.com>
2021-02-08 19:43:36 -0300

    More study

---
+++ A11/Split and Join.py
@@ -0,0 +1,8 @@
+msg ='Welcome to Python 101: Split and Join'
+
+csv = 'Eric,John,Michael,Terry,Graham'
+
+friends_list = ['Eric','John','Michael','Terry','Graham']
+
+print(msg.split())
+
---
+++ A13/Sets.py
@@ -0,0 +1,2 @@
+
+friends = {'Sasha','Rafael'}
