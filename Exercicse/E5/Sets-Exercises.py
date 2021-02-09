d96d9e253c29a15ee627062bc50758da2c58063b
Rafael Oliveira pires <rafaelo.oliveira2020@gmail.com>
2021-02-09 15:18:24 -0300

    Learn how work with sets using the (^, |, &, -)

--- Exercicse/E5/Sets-Exercises.py
+++ Exercicse/E5/Sets-Exercises.py
@@ -10,19 +10,19 @@ print('Eric' in friends and 'John' in friends)
 
 #2. combine or add the two sets 
 print(friends.union(my_friends))
-print(friends | my_friends)
+print(friends | my_friends) # Create a new list removing the copy 
 
 #3. Find names that are in both sets
 print(friends.intersection(my_friends))
-print(friends & my_friends)
+print(friends & my_friends) # Return only the duplicate values in both
 
 #4. find names that are only in friends
 print(friends.difference(my_friends))
-print(friends - my_friends)
+print(friends - my_friends) # Return the value duplicate only 
 
 #5. Show only the names who only appear in one of the lists
 print(my_friends.symmetric_difference(friends))
-print(my_friends ^ friends)
+print(my_friends ^ friends) # Create a new list with de value duplicate
 
 #6. Create a new cars-list without duplicates
 cars_no_dupl = list(set(cars))
---
+++ Study/Union_Intersection_Difference_symmetric.py
@@ -0,0 +1,11 @@
+friends = {'Rafael','Sasha','Noah'}
+
+another_friends = {'Rafael','Sasha'}
+
+print(friends ^ another_friends) # Symmetric ## Return a new set the value duplicate
+
+print(friends | another_friends) # Union ## Create a new set removing the duplicate value 
+
+print(friends & another_friends) # Intersection ## Create a new set with the duplicate value
+
+print(friends - another_friends) # Difference ## Return the duplicate Value
