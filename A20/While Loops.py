5e011043f2f0a5f0bb9e69fa87e4ac605cbceb8e
Rafael Oliveira pires <rafaelo.oliveira2020@gmail.com>
2021-02-10 18:41:47 -0300

    While loops

---
+++ A20/While Loops.py
@@ -0,0 +1,26 @@
+print("1.*Loops are great*")
+print("2.**Loops are great**")
+print("3.***Loops are great***")
+print("4.****Loops are great****")
+print("5.*****Loops are great*****")
+
+#Code before using loops
+
+i = 0
+
+while i < 5:
+    i += 1
+    print(f"{i}."+ "*"*i + "Loops are awesome" + "*"*i)
+    
+
+
+#while condition:
+    #code
+    #iterator
+# Three Loop Questions:
+#1. What do I want to repeat?
+#  -> 
+#2. What do I want to change each time?
+#  -> 
+#3. How long should we repeat?
+#  -> 
