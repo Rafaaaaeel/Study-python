3fe8d00ca2476d76a6efc9fc0c8ae6b594040a8d
Rafael Oliveira pires <rafaelo.oliveira2020@gmail.com>
2021-02-19 17:10:12 -0300

    Break

---
+++ Study/Continue and break.py
@@ -0,0 +1,21 @@
+
+#Continue
+
+for val in "string":
+    if val == "i":
+        continue
+    print(val)
+
+print("The end")
+
+print("-----------------------")
+
+#Break
+
+for val in "string":
+    if val == 'i':
+        break
+    print(val)
+
+print('the end')
+
