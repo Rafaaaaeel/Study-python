8ee0c8653b32c4f4b4872751fbf79cc5b0b81aec
Rafael <rafaelo.oliveira2020@gmail.com>
2021-04-13 06:01:55 -0300

    Fix the study folder

--- Mini projects/Create.py
+++ Mini projects/Create.py
@@ -1,12 +1,13 @@
-import os
-path = 'C:\\Users\\rafae\\Documents\\Python\\Folders'
-os.chdir(path)
+import os # 1
+path = 'C:\\Users\\rafae\\Documents\\Python\\Folders' # 1
+os.chdir(path) # 1
 
-folders = ['folder 1','folder 2','folder 3']
+folders = ['folder 1','folder 2','folder 3'] # 1
 
 #answer = input('Tell the folders names, separede by space ')
 #folders = answer.split(' ') 
 
-for i in folders:
-    os.mkdir(i)
+for i in folders: # 3
+    os.mkdir(i) # 3
+
 
--- W3S/Study/List Comprehensions.py
+++ W3S/Study/List Comprehensions.py
@@ -1,5 +1,9 @@
+import numpy as np
+
+
 a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 b = []
+
 for i in a:
     if i % 2 is 0:
         b.append(i)
