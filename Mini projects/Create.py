c54044cfd660ee5503858870d51fa257711c7beb
Rafael <rafaelo.oliveira2020@gmail.com>
2021-04-20 15:01:48 -0300

    Fixing the "Create" code, new feature

--- Mini projects/Create.py
+++ Mini projects/Create.py
@@ -1,5 +1,5 @@
 import os 
-path = 'C:\\Users\\rafae\\Documents\\Python\\Folders'
+path = 'C:\\Users\\rafae\\Documents\\Python\\'
 os.chdir(path) 
 
 folders = ['folder 1','folder 2','folder 3'] 
@@ -7,6 +7,11 @@ folders = ['folder 1','folder 2','folder 3']
 #answer = input('Tell the folders names, separede by space ')
 #folders = answer.split(' ') 
 
-for i in folders : os.mkdir(i) 
+for i in folders : 
+    if os.path.exists(i):
+        print(f'File does exist {i}')
+        continue
+    os.mkdir(i)
+    print(f'Create: {i}')
 
 
