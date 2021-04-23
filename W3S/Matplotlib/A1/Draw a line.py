5e919aa13e6ad6e3ffd42a08cea2bfc5c997a3dd
Rafael <rafaelo.oliveira2020@gmail.com>
2021-04-23 09:28:54 -0300

    Learn some plot codes

---
+++ W3S/Matplotlib/A1/Draw a line.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0,6])
+print(xpoints)
+ypoints = np.array([0,250])
+print(ypoints)
+
+plt.plot(xpoints, ypoints)
+plt.show()
